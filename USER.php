7b5345e1e14b333dbfabd7c9505bfb6f82471659
DerrelGerary <72934778+DerrelGerary@users.noreply.github.com>
2021-06-08 20:33:00 +0700

    update

---
+++ USER.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Halaman User - www.malasngoding.com</title>
+</head>
+<body>
+	<?php 
+	session_start();
+ 
+	if($_SESSION['level']==""){
+		header("location:index.php?pesan=gagal");
+	}
+ 
+	?>
+	<h1>Halaman Pegawai</h1>
+ 
+	<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</p>
+	<a href="ASU.php">LOGOUT</a>
+ 
+	<br/>
+	<br/>
+ 
+	<a><a href="https://www.malasngoding.com/membuat-login-multi-user-level-dengan-php-dan-mysqli">Membuat Login Multi Level Dengan PHP</a> - www.malasngoding.com</a>
+</body>
+</html>
