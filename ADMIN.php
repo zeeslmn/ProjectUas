68fa1789af5386411d5d5c997f7411f8acd3356b
zeeslmn <76543509+zeeslmn@users.noreply.github.com>
2021-06-09 00:15:50 +0700

    Update ADMIN.php

--- ADMIN.php
+++ ADMIN.php
@@ -6,8 +6,7 @@
 <body>
 	<?php 
 	session_start();
- 
-	// cek apakah yang mengakses halaman ini sudah login
+
 	if($_SESSION['level']==""){
 		header("location:INDEX.php?pesan=gagal");
 	}
@@ -15,7 +14,7 @@
 	?>
 	<h1>Halaman Admin</h1>
  
-	<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</p>
+	<p>Halo <b><?php echo $_SESSION['username']; ?></b> You have logeed in as  <b><?php echo $_SESSION['level']; ?></b>.</p>
 	<a href="LOGOUT.php">LOGOUT</a>
  
 	<br/>
